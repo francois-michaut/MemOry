2ea9ec1285bf291bff28e5a4633c626791681dc3
Benoit Gacher <benoitgacher@gmail.com>
2022-10-27 13:16:42 +0200

    all card are generate with different id and background image

--- app/js/gamePlan.js
+++ app/js/gamePlan.js
@@ -2,9 +2,48 @@ const gamePlan = {
 
 init: function() {
 
-    for ( let i=0 ; i<32 ; i++) {
+    const imagesArray= [
+        'airplane',
+        'basket',
+        'bus',
+        'carrot',
+        'cow',
+        'dog',
+        'firefighter',
+        'fir',
+        'foot',
+        'giraffe',
+        'helicopter',
+        'lego',
+        'lion',
+        'pinata',
+        'pokemon',
+        'rocket'
+    ];
+
+    for (const image of imagesArray ) {
         
-         card.init(); 
+         card.init();
+// Récupération du dernier élément d'un tableau   
+//https://bobbyhadz.com/blog/javascript-get-last-element-with-class
+         const cardBackElement = Array.from(document.querySelectorAll('.card__back')).pop();
+
+         cardBackElement.style.backgroundImage = `url(../assets/images/${image}.png)`; 
+
+         cardBackElement.id = `${image}-1`;
+
+
+         card.init();
+         // Récupération du dernier élément d'un tableau   
+         //https://bobbyhadz.com/blog/javascript-get-last-element-with-class
+         const cardBackElement2 = Array.from(document.querySelectorAll('.card__back')).pop();
+         
+         cardBackElement2.style.backgroundImage = `url(../assets/images/${image}.png)`; 
+         
+         cardBackElement2.id = `${image}-2`;
+
+         
+         
     }
 
 },
