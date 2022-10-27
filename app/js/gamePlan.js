63c7287631ed80f2ae9ae7165533893e7f6cf51c
Benoit Gacher <benoitgacher@gmail.com>
2022-10-27 14:09:53 +0200

    all cards are generated randomly

--- app/js/gamePlan.js
+++ app/js/gamePlan.js
@@ -3,23 +3,41 @@ const gamePlan = {
 init: function() {
 
     const imagesArray= [
-        'airplane',
-        'basket',
-        'bus',
-        'carrot',
-        'cow',
-        'dog',
-        'firefighter',
-        'fir',
-        'foot',
-        'giraffe',
-        'helicopter',
-        'lego',
-        'lion',
-        'pinata',
-        'pokemon',
-        'rocket'
+        'airplane-1',
+        'airplane-2',
+        'basket-1',
+        'basket-2',
+        'bus-1',
+        'bus-2',
+        'carrot-1',
+        'carrot-2',
+        'cow-1',
+        'cow-2',
+        'dog-1',
+        'dog-2',
+        'firefighter-1',
+        'firefighter-2',
+        'fir-1',
+        'fir-2',
+        'foot-1',
+        'foot-2',
+        'giraffe-1',
+        'giraffe-2',
+        'helicopter-1',
+        'helicopter-2',
+        'lego-1',
+        'lego-2',
+        'lion-1',
+        'lion-2',
+        'pinata-1',
+        'pinata-2',
+        'pokemon-1',
+        'pokemon-2',
+        'rocket-1',
+        'rocket-2'
     ];
+    
+    this.randomizeCard(imagesArray);
 
     for (const image of imagesArray ) {
         
@@ -30,22 +48,34 @@ init: function() {
 
          cardBackElement.style.backgroundImage = `url(../assets/images/${image}.png)`; 
 
-         cardBackElement.id = `${image}-1`;
+         cardBackElement.id = `${image}`;
 
 
-         card.init();
+        // card.init();
          // Récupération du dernier élément d'un tableau   
          //https://bobbyhadz.com/blog/javascript-get-last-element-with-class
-         const cardBackElement2 = Array.from(document.querySelectorAll('.card__back')).pop();
+       //  const cardBackElement2 = Array.from(document.querySelectorAll('.card__back')).pop();
          
-         cardBackElement2.style.backgroundImage = `url(../assets/images/${image}.png)`; 
+        // cardBackElement2.style.backgroundImage = `url(../assets/images/${image}.png)`; 
          
-         cardBackElement2.id = `${image}-2`;
+        //  cardBackElement2.id = `${image}-2`;
 
          
          
     }
 
 },
+//https://www.astucesmobiles.com/comment-melanger-un-tableau-en-javascript/
+randomizeCard: function(array){
+// renvoie la longeur du tableau , soit 32 images
+    let i = array.length;
+// Ont commence notre itération à la fin du tableau
+    while (--i > 0) {
+
+        let randIndex = Math.floor(Math.random() * (i+1));
+        [array[randIndex],array[i]] = [array[i], array[randIndex]];
+    }
+    return array;
+}
 
 }
